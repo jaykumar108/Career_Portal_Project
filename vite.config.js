0044ae271f5ad36cfed533463fb76d432ce980da
Jay Kumar Sharma <157815432+jaykumar108@users.noreply.github.com>
2025-05-22 23:03:29 +0530

    fixced error 2

--- vite.config.js
+++ vite.config.js
@@ -4,20 +4,23 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/',
   build: {
     outDir: 'dist',
     assetsDir: 'assets',
+    sourcemap: true,
     rollupOptions: {
       output: {
         manualChunks: undefined,
-        entryFileNames: 'assets/[name].js',
-        chunkFileNames: 'assets/[name].js',
-        assetFileNames: 'assets/[name].[ext]'
+        entryFileNames: 'assets/[name].[hash].js',
+        chunkFileNames: 'assets/[name].[hash].js',
+        assetFileNames: 'assets/[name].[hash].[ext]'
       }
     }
   },
   server: {
     port: 3000,
-    open: true
+    open: true,
+    host: true
   }
 }) 
