83f73dba6fa78dabe1c49c0c7e8d7c278b074b8a
Jay Kumar Sharma <157815432+jaykumar108@users.noreply.github.com>
2025-06-25 10:52:15 +0530

    created vercel.json file

--- backend/utils/supabaseClient.js
+++ backend/utils/supabaseClient.js
@@ -1,7 +1,8 @@
 const { createClient } = require('@supabase/supabase-js');
 
-const supabaseUrl = 'https://chpqznnwxaewksfxfqmk.supabase.co';
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImNocHF6bm53eGFld2tzZnhmcW1rIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDIxODk0NDcsImV4cCI6MjA1Nzc2NTQ0N30.7O6Q9F8wmLbFqxApDr3xvaD7R1ppmanSCZsJQqPTWvg'; // Service role key for backend
+
+const supabaseUrl = process.env.SUPABASE_URL;
+const supabaseKey = process.env.SUPABASE_SERVICE_KEY; // Service role key for backend
 const supabase = createClient(supabaseUrl, supabaseKey);
 
 module.exports = supabase; 
